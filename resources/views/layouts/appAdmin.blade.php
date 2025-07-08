5fa2154ad05ca0d7550a4ee4decfcd6328a20e6a
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-07-07 23:23:38 -0500

    Plantilla para el administrador

--- resources/views/layouts/appAdmin.blade.php
+++ resources/views/layouts/appAdmin.blade.php
@@ -108,9 +108,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
-                        <a href="{{ route('zonas-seguras.index') }}" class="nav-item nav-link active">Zonas Seguras</a>
-                        <a href="{{ route('ZonasRiesgo.index') }}" class="nav-item nav-link">Zonas de Riesgo</a>
-                        <a href="{{ route('admin.puntos-encuentro.index') }}" class="nav-item nav-link">Puntos de Encuentro</a>
+                        <a href="{{ route('zonas-seguras.index') }}" class="nav-item nav-link {{ request()->routeIs('zonas-seguras.index') ? 'active' : '' }}">Zonas Seguras</a>
+                        <a href="{{ route('ZonasRiesgo.index') }}" class="nav-item nav-link {{ request()->routeIs('ZonasRiesgo.index') ? 'active' : '' }}">Zonas de Riesgo</a>
+                        <a href="{{ route('admin.puntos-encuentro.index') }}" class="nav-item nav-link {{ request()->routeIs('admin.puntos-encuentro.index') ? 'active' : '' }}">Puntos de Encuentro</a>
                     </div>
                 </div>
             </nav>
