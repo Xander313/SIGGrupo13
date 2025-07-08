b1a27ca9748582df71a9d7acaec0b961ddfc3254
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-07-07 21:16:30 -0500

    Se agregaron los campos para las migraciones

--- database/migrations/2025_07_08_020521_create_riesgos_table.php
+++ database/migrations/2025_07_08_020521_create_riesgos_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('riesgos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("nombre");
+            $table->string("descripcion");
+            $table->string("nivel");
+            $table->double("latitud1");
+            $table->double("longitud1");
+            $table->double("latitud2");
+            $table->double("longitud2");
+            $table->double("latitud3");
+            $table->double("longitud3");
+            $table->double("latitud4");
+            $table->double("longitud4");
         });
     }
 
